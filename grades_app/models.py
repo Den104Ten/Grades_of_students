092ece451c1a2987d39c96d6c3ed29146b396933
Den104Ten <dan_eltsov@mail.ru>
2023-11-10 22:48:46 +0300

    coom from win

--- grades_app/models.py
+++ grades_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Students(models.Model):
     FIO = models.CharField(max_length=100)
     DOB = models.CharField(max_length=50)
-    Start_year = models.DateField()
+    Start_year = models.IntegerField()
     Course = models.IntegerField()
     Group = models.IntegerField()
     First_year = models.IntegerField()
@@ -13,4 +13,13 @@ class Students(models.Model):
     Third_year = models.IntegerField()
     Fourth_year = models.IntegerField()
 
-
+    def average_grade(self):
+        if self.Second_year == 0:
+            total_grades = self.First_year
+        elif self.Third_year == 0:
+            total_grades = (self.First_year + self.Second_year) / 2
+        elif self.Fourth_year == 0:
+            total_grades = (self.First_year + self.Second_year + self.Third_year) / 3
+        else:
+            total_grades = (self.First_year + self.Second_year + self.Third_year + self.Fourth_year) / 4
+        return total_grades
--- grades_app/tests.py
+++ grades_app/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from .views import *
+from .models import *
+from django.urls import reverse
 
-# Create your tests here.
+
+# Тесты!!!
+
+# Первый тест проверяет правильный ли статус код выдает страничка
+
+# Второй тест проверяет корректно ли отображаются студенты на страничке
+
+# Третий тест проверяет корректно ли вычисляется средний балл для студентов
+
+# Во всех тестах были созданы искуственные студенты для проверки правильности обработки данных
+
+class TestHomeView(TestCase):
+    
+    # 1 Test
+    def test_HomeView(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+
+    # 2 Test
+    def test_student_list_displayed(self):
+        # Test_examples
+        student1 = Students.objects.create(FIO='Карасев Марк Даниэльевич', DOB='23.08.2004', Start_year=2022, Course=2, Group=8294, First_year=5, Second_year=3, Third_year=0, Fourth_year=0)
+        
+        url = reverse('home')
+        response = self.client.get(url)
+        
+        self.assertTemplateUsed(response, 'grades_app/home.html')
+        self.assertContains(response, student1.FIO)
+        
+    # 3 Test
+    def test_average_grade_calculation(self):
+        # Test examples
+        student1 = Students.objects.create(FIO='Карасев Марк Даниэльевич', DOB='23.08.2004', Start_year=2022, Course=2,
+                                           Group=8294, First_year=5, Second_year=3, Third_year=0, Fourth_year=0)
+
+        url = reverse('home')
+        response = self.client.get(url)
+
+        self.assertTemplateUsed(response, 'grades_app/home.html')
+        # Проверяем, что средняя оценка для каждого студента вычисляется правильно. До двух знаков после запятой
+        self.assertAlmostEqual(student1.average_grade(), 4, places=2)
+
+
